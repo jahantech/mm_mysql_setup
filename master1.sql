a071794f7d1e1469e367e53aa4293c7b1927d930
jahantech <jahansyed@hotmail.co.uk>
2016-03-22 20:40:44 +0000

    As both instances are using on the same box , we can root user for testing

--- master1.sql
+++ master1.sql
@@ -1,6 +1,3 @@
-create user 'replicator'@'%' ;
-grant replication slave on *.* to 'replicator'@'%'; 
-flush privileges;
 create database example; 
-CHANGE MASTER TO MASTER_HOST = '127.0.0.1', MASTER_USER = 'replicator', MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 107, MASTER_PORT=3308; 
-slave start;  
+CHANGE MASTER TO MASTER_HOST = '127.0.0.1', MASTER_USER = 'root', MASTER_LOG_FILE = 'mysql-bin.000001', MASTER_LOG_POS = 107, MASTER_PORT=3308; 
+start slave; 
